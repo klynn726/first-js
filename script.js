620b721193f26cdde6945f27aa6327a494c9858b
Kate <klynn726@yahoo.com>
2020-12-05 10:47:30 -0700

    first save

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction()
+ {
+    onclick= getElementById("button1").width = "300 px";
+    onclick= getElementById("button2").style.color = "blue"
+    onclick= getElementById("button3").style.opacity = "0.5"
+    onclick= getElementById("button4").reset
+}
